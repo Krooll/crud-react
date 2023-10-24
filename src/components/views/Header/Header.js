f352bf4ad68565ea8befcc2d2d86c76b453785a6
Pawel Krol <paullkroll@icloud.com>
2023-10-24 16:33:50 +0200

    Add scss devs

--- src/components/views/Header/Header.js
+++ src/components/views/Header/Header.js
@@ -1,11 +1,22 @@
 import styles from './Header.module.scss';
+import { Navbar, Col, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return(
-        <div>
-            <h1>Header</h1>
-            <p>Work in progress</p>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg='primary' className={styles.navBar}>
+                <Col>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                </Col>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        <Nav.Link >More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                        Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
     );
 };
 
